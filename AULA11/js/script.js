fcba79b71f405d9f33c1f26e38dc833785445671
Isakiel Souza <isakielsouza@outlook.com.br>
2020-08-13 01:21:11 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA11/js/script.js
@@ -0,0 +1,9 @@
+let numero1 = window.prompt("Digite um nṹmero:");
+let numero2 = window.prompt("Digite outro número:");
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+const  resultado = numero1 + numero2
+
+window.alert(`O resutlado da sua conta é: "${resultado}`);
