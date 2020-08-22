84b4c0554cb9bb9f0e771f70fbfffe5f930e8fdc
Isakiel Souza <isakielsouza@outlook.com.br>
2020-08-22 16:46:58 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA16/index.js
@@ -0,0 +1,32 @@
+//              0       1         2             3         4
+const alunos = ['Luiz', 'Maria', 'Isakiel'];//,'Luiza', 'Eduardo'
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(2, 1));//fatiando array
+
+
+
+// console.log(alunos[50]) //undefined: indece nao existe
+
+// delete alunos[1]; ///deletando elemento e seu indice,
+
+// const removidoDoInicio = alunos.shift(); //removido do inicio
+//console.log(removidoDoInicio);
+
+//const removidoDoFinal = alunos.pop(); //remove o ultimo
+//console.log(removidoDoFinal);
+
+// alunos.unshift('Leticia'); // adiciona no inicio do array
+
+// alunos.push('Leticia'); // adiciona no final do array
+
+// alunos[1] = 'Eduardo'
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
---
+++ AULA17/index.js
@@ -0,0 +1,47 @@
+const raiz = n => n ** 0.5;
+
+
+// const raiz = (n) => {
+//   return n ** 0.5;
+// };
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+const hello = () => {
+  console.log(`hello`)
+}
+
+hello();
+
+// const raiz = function (n) {
+//   return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(25));
+
+
+//function soma(x, y) {
+
+// function soma(x = 1, y = 1) {
+//   return x + y;
+// }
+
+// console.log(soma());
+
+// console.log(soma(5,10));
+
+// console.log(soma("Isakiel", " Souza"));
+
+
+
+// function saudacao(nome) {
+//   return `bom dia ${nome}`
+// }
+
+// const variavel = saudacao(`Luiz`)
+
+
+// console.log(variavel)
