1cc0d45e33e5152f0f6fcf7a73b2e7bd6b556da2
Isakiel Souza <isakielsouza@outlook.com.br>
2020-08-27 23:28:13 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA25/index.js
@@ -0,0 +1,16 @@
+/**
+ * IF - ELSE IF - ELSE
+ * O numero esta Entre?
+ */
+
+const numero = 100;
+
+if (numero >= 0 && numero <=5) {
+  console.log("O numero está entre 0 e 5.");
+} else if (numero >= 6 && numero <= 8) {
+  console.log("O numero está entre 6 e 8.");
+} else if (numero >= 9 && numero <= 11) {
+  console.log("O numero está entre 9 e 11.");
+} else {
+  console.log("O numero não está entre 0 e 11.")
+}
