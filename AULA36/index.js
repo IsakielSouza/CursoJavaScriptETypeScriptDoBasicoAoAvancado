27d8099604d39be76648948044ad14663db70417
Isakiel Souza <isakielsouza@outlook.com.br>
2020-09-03 17:40:19 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA36/index.js
@@ -0,0 +1,11 @@
+const frutas = ['Pêra', 'Uva', 'Maçã'];
+//* 2° FOR
+for ( let fruta in frutas) {
+  console.log(frutas[fruta]);
+}
+
+
+//* 1° FOR
+// for (let i = 0; i < frutas.length; i++) {
+//   console.log(frutas[i]);
+// }
