1682b981c107a41dc636d2f36fe08695a90d5640
Isakiel Souza <isakielsouza@outlook.com.br>
2020-09-03 09:00:29 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA34/index.js
@@ -0,0 +1,10 @@
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i=0; i < frutas.length; i++) {
+  console.log(`Índice ${i}`,frutas[i])
+}
+
+// for (let i = 0; i <= 5;i++) {
+//   const par = i % 2 == 0 ? 'Par' : 'Impar'
+//   console.log(i, par)
+// }
