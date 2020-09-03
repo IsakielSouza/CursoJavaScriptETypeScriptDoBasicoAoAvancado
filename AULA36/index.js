c2a5fcd066b4ca78983a80097f050e0c743bf0a9
Isakiel Souza <isakielsouza@outlook.com.br>
2020-09-03 18:43:37 -0300

    Curso JS e TS do básico ao avançado

--- AULA36/index.js
+++ AULA36/index.js
@@ -1,11 +1,32 @@
-const frutas = ['Pêra', 'Uva', 'Maçã'];
-//* 2° FOR
-for ( let fruta in frutas) {
-  console.log(frutas[fruta]);
+//FOR IN  - > lER OS INDICES OU CHAVES DO OBJETO
+
+const pessoa = {
+  nome: 'Isakiel',
+  sobrenome: 'Souza',
+  idade: 26
 }
+
+for (let chave in pessoa) {
+  console.log(chave, pessoa[chave]);
+};
+
+// const chave = 'nome'
+
+// console.log(pessoa[chave]);
+// console.log(pessoa['nome']);
+// console.log(pessoa.nome);
+
+
+
+// }
+// const frutas = ['Pêra', 'Uva', 'Maçã'];
+// //* 2° FOR
+// for ( let fruta in frutas) {
+//   console.log(frutas[fruta]);
+// }
 
 
 //* 1° FOR
 // for (let i = 0; i < frutas.length; i++) {
 //   console.log(frutas[i]);
-// }
+// 
