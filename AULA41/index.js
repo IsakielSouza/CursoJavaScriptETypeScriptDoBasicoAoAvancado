8311e856535764499dc6a311e249409e3f97ea05
Isakiel Souza <isakielsouza@outlook.com.br>
2020-09-11 17:29:30 -0300

    Curso JS e TS do básico ao avançado

---
+++ AULA41/index.js
@@ -0,0 +1,25 @@
+// //Escreva uma frunção que recebe 2 numeros e retorna o maior deles
+// function maior(x, y) {
+//   return Math.max(x, y);
+// }
+
+const maior = (x,y) => x > y ? x : y;
+
+
+// const maior = (x,y) => {
+//   return x > y ? x : y;
+// }
+
+// function maior(x,y) {
+//   return x > y ? x : y;
+// }
+ 
+
+// function maior(x,y) {
+//   if (x > y) {
+//     return x;
+//   }
+//     return y;
+// }
+ 
+console.log(maior(100,20))
---
+++ AULA43/index.js
@@ -0,0 +1,9 @@
+/**
+ * Escreva uma função que receba um número e retorne o seguinte:
+ * Número é divisível por 3 = Fizz
+ * Número é divisível por 5 = Buzz
+ * Número é divisível por 3 e 5 = FizzBuzz
+ * Número NÃO é divisível por 3 e 5 = retorna o próprio número
+ * Checa se o número é realmente um número = retorna o valor
+ * Use a funnção com Números de 0 a 100
+ */
