8cb95fff34a3cc33b38f0c6d3a334ac3aa2b60b9
Isakiel Souza <isakielsouza@outlook.com.br>
2020-09-15 02:14:29 -0300

    Curso JS e TS do básico ao avançado

--- AULA47/assets/js/main.js
+++ AULA47/assets/js/main.js
@@ -1,38 +1,46 @@
-function criaHoradosSegundos(seconds) {
-  const data = new Date(seconds * 1000);
-  return data.toLocaleTimeString('pt-BR', {
-    hour12: false,
-    timeZone: 'UTC'
+function relogio () {
+  function criaHoradosSegundos(seconds) {
+    const data = new Date(seconds * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'UTC'
+    });
+  }
+  
+  const relogio = document.querySelector('.relogio');
+  
+  let segundos = 0;
+  let timer;
+  
+  function startRelogio() {
+    timer = setInterval(function() {
+      segundos++
+      relogio.innerHTML = criaHoradosSegundos(segundos);
+    }, 1000);
+  }
+  
+  document.addEventListener('click', function(evento) {
+    const elemento = evento.target;
+    
+    if(elemento.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado')
+      segundos = 0;
+  
+    }
+    
+    if(elemento.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado')
+      clearInterval(timer);
+      startRelogio();
+    }
+  
+    if(elemento.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+  
   });
 }
-
-
-const relogio = document.querySelector('.relogio');
-const inicar = document.querySelector('.inicar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-
-let segundos = 0;
-let time;
-
-function startRelogio() {
-  time = setInterval(function() {
-    segundos++
-    relogio.innerHTML = criaHoradosSegundos(segundos);
-  }, 1000);
-}
-
-inicar.addEventListener('click', function(event) {
-  clearInterval(time);
-  startRelogio();
-});
-
-pausar.addEventListener('click', function(event) {
-  clearInterval(time);
-});
-
-zerar.addEventListener('click', function(event) {
-  clearInterval(time);
-  relogio.innerHTML = '00:00:00'
-  segundos = 0;
-});
+relogio();
